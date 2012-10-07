70103ba23fe5aed30d9f5b9d8677bbd81435c332
David Deryl Downey <me@daviddwdowney.com>
2012-10-07 19:03:45 -0400

    All DTF gems need to include at least the core models from the dtf master gem, and the matching schema. This commit adds them, and updates the baseline schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,38 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120616203436) do
+
+  create_table "analysis_cases", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "verification_suite_id"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+  end
+
+  create_table "case_tests", :force => true do |t|
+    t.string   "description"
+    t.text     "cmd"
+    t.integer  "analysis_case_id"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "full_name"
+    t.string   "user_name"
+    t.string   "email_address"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  create_table "verification_suites", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
 end
