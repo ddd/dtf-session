37039ed03ea13d88e6ee872d6db5e1c0dbbfd457
David Deryl Downey <me@daviddwdowney.com>
2012-10-07 10:37:59 -0400

    Deleted unneeded files for gh-pages branch. None of the project files are needed for display on the project page

--- lib/dtf-session.rb
+++
@@ -1,7 +0,0 @@
-require "dtf-session/version"
-
-module Dtf
-  module Session
-    # Your code goes here...
-  end
-end
--- lib/dtf-session/version.rb
+++
@@ -1,5 +0,0 @@
-module Dtf
-  module Session
-    VERSION = "0.0.1"
-  end
-end
