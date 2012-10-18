dd686c5c1eb8fdaec638a01c030d519df3a611f2
David Deryl Downey <me@daviddwdowney.com>
2012-10-18 16:03:09 -0400

    Added environment_spec.rb and 'session' load location in dtf-session.rb module file.

--- lib/dtf-session.rb
+++ lib/dtf-session.rb
@@ -1,11 +1,36 @@
 require "dtf-session/version"
 require 'thor'
-require 'session'
 
-module Dtf
 load "#{File.join(File.dirname(__FILE__), "/config/environment.rb")}"
 
+# TODO: Add fields like env_initial from testrunner to CaseTest
+
+module Dtf
+
   module Session
-    # Your code goes here...
+    require 'session'
+
+
+    class Environment
+
+      def self.env_to_hash(env_string)
+        lines = env_string.split("\n")
+        key_value_pairs = lines.map { |line|
+          key, value = *line.split("=", 2)
+          [key.to_sym, value]
+        }
+
+        Hash[key_value_pairs]
+      end
+
+      def self.create_shell
+        ::Session.new
+      end
+
+      def self.set_debug(shell)
+        shell.debug = true
+      end
+
+    end
   end
 end
