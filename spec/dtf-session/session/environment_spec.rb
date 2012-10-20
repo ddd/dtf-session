d04aba92f1dce7e22cdef18d72a0d000d628cd89
David Deryl Downey <me@daviddwdowney.com>
2012-10-19 22:06:35 -0400

    Forgot to remove empty Shell class test when I removed the class. Empty so the test passed. Fixes that.

--- spec/dtf-session/session/environment_spec.rb
+++ spec/dtf-session/session/environment_spec.rb
@@ -3,16 +3,11 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe "Environment" do
 
   context "Class" do
+
     it "should be a Class" do
       Dtf::Session::Environment.class.should eq(Class)
     end
-  end
-
-  context "Shell" do
-
-    it "should be a Class" do
-    end
-
+  
   end
 
 end
