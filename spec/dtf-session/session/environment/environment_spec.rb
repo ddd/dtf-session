d3abb05d054ef4653aa3e5844bb9f7c891863311
David Deryl Downey <me@daviddwdowney.com>
2012-10-19 23:30:21 -0400

    Modified environment_spec.rb spec_helper load line to match the distance it is relative to the actual spec_helper file

--- spec/dtf-session/session/environment/environment_spec.rb
+++ spec/dtf-session/session/environment/environment_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
 
 describe "Environment" do
 
@@ -7,7 +7,7 @@ describe "Environment" do
     it "should be a Class" do
       Dtf::Session::Environment.class.should eq(Class)
     end
-  
+
   end
 
 end
