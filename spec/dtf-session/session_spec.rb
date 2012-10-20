515274c2e9c6c9b8425922f6ad373cd034d31634
David Deryl Downey <me@daviddwdowney.com>
2012-10-19 21:52:06 -0400

    Added context to check that module contains defined classes. This also documents the classes Dtf::Session is expected to deliver

--- spec/dtf-session/session_spec.rb
+++ spec/dtf-session/session_spec.rb
@@ -1,8 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Dtf::Session do
-  
+
   context "Module" do
+
     it "should be a Module" do
       Dtf::Session.class.should eq(Module)
     end
@@ -11,13 +12,14 @@ describe Dtf::Session do
       Dtf::Session.class.const_defined?(:VERSION)
       Dtf::Session::VERSION.should_not be_empty
       Dtf::Session::VERSION.class.should eq(String)
-    end 
+    end
+
   end
-  
+
   context "Dependencies" do
+
     it "should depend on DTF master gem" do
       @gem_dep = false
-
       Gem.loaded_specs['dtf-session'].dependencies.each do |spec|
         if spec.name == "dtf"
           @gem_dep = true
@@ -26,6 +28,16 @@ describe Dtf::Session do
 
       @gem_dep.should be_true
     end
+
+  end
+
+  context "Contained Classes" do
+
+    it "should contain Environment class" do
+      Dtf::Session.constants.include?(:Environment)
+    end
+
   end
 
 end
+
